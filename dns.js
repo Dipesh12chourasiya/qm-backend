35718b3e550a92346f73605f31c9cc6b9364ce79
Dipesh <dipeshchourasiya12@gmail.com>
2026-05-31 13:03:02 +0530

    fff

--- dns.js
+++
@@ -1,9 +0,0 @@
-const dns = require("dns");
-
-dns.resolveSrv(
-  "_mongodb._tcp.clusterquize0.ga3ejom.mongodb.net",
-  (err, records) => {
-    console.log("Error:", err);
-    console.log("Records:", records);
-  }
-);
