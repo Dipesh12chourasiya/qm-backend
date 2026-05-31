3e783b3f49897d33efe869df0757742611d8a81b
Dipesh <dipeshchourasiya12@gmail.com>
2026-05-31 13:35:28 +0530

    fixed db.js

--- config/db.js
+++ config/db.js
@@ -1,15 +1,24 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
 
 const connectDB = async () => {
-  // console.log("MONGO_URI:", process.env.MONGO_URI);
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB Connected");
-  } catch (error) {
-    // console.error("MongoDB Error:", error.message);
-    console.error(error);
-    process.exit(1);
+  if (cached.conn) {
+    return cached.conn;
   }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(process.env.MONGO_URI, {
+      bufferCommands: false,
+    });
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
 };
 
 module.exports = connectDB;
